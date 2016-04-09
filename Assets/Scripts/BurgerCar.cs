e826a21c63ae9616cc78b872272ed83a2c42a160
andresg6 <andresg6@uci.edu>
2016-04-09 15:32:01 -0700

    Added PlayerClip layer to physics; Layer that only affects the player

--- Assets/Scripts/BurgerCar.cs
+++ Assets/Scripts/BurgerCar.cs
@@ -5,10 +5,18 @@ public class BurgerCar : MonoBehaviour {
     public Vector2 direction; //should be normalized
     public float speed;
 
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void FixedUpdate()
     {
         
         transform.Translate(direction.normalized * speed * Time.deltaTime);
+        rb.velocity = new Vector2(0,0);
     }
 
 
