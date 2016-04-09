06f63d370a507e791cfd28f5052c32b7c6e54f8c
Kevin Wong <wong.kevin47@gmail.com>
2016-04-08 22:12:01 -0700

    Add Lerping to FollowGaze

--- Assets/Scripts/FollowGaze.cs
+++ Assets/Scripts/FollowGaze.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class FollowGaze : MonoBehaviour {
 
+    public float lerpAmount;
+
     private GazePointDataComponent gazePointDataComponent;
 
 	void Start () {
@@ -15,7 +17,10 @@ public class FollowGaze : MonoBehaviour {
         if (lastGazePoint.IsWithinScreenBounds)
         {
             Vector2 screenSpace = lastGazePoint.Screen;
-            transform.position = Camera.main.ScreenToWorldPoint(new Vector3(screenSpace.x, screenSpace.y, Camera.main.nearClipPlane));
+
+            Vector3 gazeWorldPoint = Camera.main.ScreenToWorldPoint(new Vector3(screenSpace.x, screenSpace.y, Camera.main.nearClipPlane));
+            Vector3 lerped = Vector3.Lerp(transform.position, gazeWorldPoint, lerpAmount);
+            transform.position = lerped;
         }
 	}
 }
