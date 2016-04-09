7ae74af48e63e8e1508cdf709dfd43aa905c9baa
Natalie Nygaard <natalie42@me.com>
2016-04-09 14:01:15 -0700

    the you tried scene

---
+++ Assets/Scripts/GetScore.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GetScore : MonoBehaviour {
+
+	public int score; // score from the game that just ended
+
+	public Text scoreText;
+	public Text highScoreText;
+
+	void Awake() {
+		if (score > PlayerPrefs.GetInt ("HighScore")) {
+			PlayerPrefs.SetInt ("HighScore", score);
+		}
+	}
+
+	void Start () {
+		scoreText.text = "Distance Traveled: " + score;
+		highScoreText.text = "High Score: " + PlayerPrefs.GetInt ("HighScore");
+	}
+}
