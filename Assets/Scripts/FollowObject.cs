7b7557c5ef753984377875c9c0c9bf4709d06698
Kevin Wong <wong.kevin47@gmail.com>
2016-04-08 23:04:30 -0700

    Add following a cat

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowObject : MonoBehaviour {
+
+    public GameObject objectToFollow;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, objectToFollow.transform.position, step);
+    }
+}
