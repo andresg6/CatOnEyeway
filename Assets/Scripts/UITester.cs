a99f461342eed8dfdb44922179c39f06a76dcf8b
andresg6 <andresg6@uci.edu>
2016-04-08 22:58:59 -0700

    Basic UI created, Updates Lives & Distance

---
+++ Assets/Scripts/UITester.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UITester : MonoBehaviour {
+    public UIScript mainUI;
+
+    private int lives;
+    private float distance;
+
+	// Use this for initialization
+	void Start () {
+        lives = 10;
+        distance = 1200.0f;
+        mainUI.updateLives(lives);
+        mainUI.updateDistanceTraveled(distance);
+	}
+
+    public void IncreaseLives()
+    {
+        lives++;
+        mainUI.updateLives(lives);
+    }
+
+    public void DecreaseLives()
+    {
+        lives--;
+        mainUI.updateLives(lives);
+    }
+
+    public void IncreaseDistance()
+    {
+        distance += 100.005f;
+        mainUI.updateDistanceTraveled(distance);
+    }
+
+    public void DecreaseDistance()
+    {
+        distance -= 100.005f;
+        mainUI.updateDistanceTraveled(distance);
+    }
+}
