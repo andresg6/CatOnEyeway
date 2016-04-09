579b15cacc4c3ff51557ff89cd877e9bd64036c7
Kevin Wong <wong.kevin47@gmail.com>
2016-04-09 12:35:10 -0700

    Add cat health

---
+++ Assets/Scripts/CatHealth.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatHealth : MonoBehaviour {
+
+    public int maxLives;
+    private int currentLives;
+
+    public GameObject deathSprite;
+
+	void Start () {
+        currentLives = maxLives;
+	}
+	
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        currentLives--;
+        Debug.Log("Current Lives: " + currentLives);
+        if (currentLives <= 0)
+        {
+            Destroy(gameObject);
+            Debug.Log("cat died");
+        }
+    }
+
+    void OnDestroy()
+    {
+        Instantiate(deathSprite, transform.position, Quaternion.identity);
+    }
+}
