f1200bfc41f4d3a2bd5d74fe2f9789894ae10dcf
andresg6 <andresg6@uci.edu>
2016-04-09 15:56:36 -0700

    SceneSwapper prefab; Menu StartGame works

---
+++ Assets/Scripts/SceneSwap.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSwap : MonoBehaviour {
+    public int MenuScene = 0;
+    public int GameScene = 1;
+    public int OverScene = 2; 
+	
+	public void LoadMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(MenuScene);
+    }
+
+    public void LoadGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(GameScene);
+    }
+
+    public void LoadGameOver()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(OverScene);
+    }
+}
