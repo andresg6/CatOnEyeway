0fc0b7a2d717adf54aad76cf5d402ee835814a14
Kevin Wong <wong.kevin47@gmail.com>
2016-04-08 21:39:01 -0700

    Add FollowGaze script

---
+++ Assets/Scripts/FollowGaze.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowGaze : MonoBehaviour {
+
+    private GazePointDataComponent gazePointDataComponent;
+
+	void Start () {
+        gazePointDataComponent = GetComponent<GazePointDataComponent>();
+	}
+	
+	void Update () {
+        EyeXGazePoint lastGazePoint = gazePointDataComponent.LastGazePoint;
+
+        if (lastGazePoint.IsWithinScreenBounds)
+        {
+            Vector2 screenSpace = lastGazePoint.Screen;
+            transform.position = Camera.main.ScreenToWorldPoint(new Vector3(screenSpace.x, screenSpace.y, Camera.main.nearClipPlane));
+        }
+	}
+}
