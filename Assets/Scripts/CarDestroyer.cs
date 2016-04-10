f22bbee0636b1140f95bb5d3750ea266b01c8527
andresg6 <andresg6@uci.edu>
2016-04-09 22:38:10 -0700

    Fixed clipping in MainMenu; Cars are deleted after a certain distance

---
+++ Assets/Scripts/CarDestroyer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarDestroyer : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Car"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
