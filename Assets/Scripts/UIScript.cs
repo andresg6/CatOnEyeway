212341b0f5bf77d8c5fe650ce5f4745839a72ec8
andresg6 <andresg6@uci.edu>
2016-04-09 18:11:19 -0700

    Pause menu added to UI

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -6,6 +6,57 @@ public class UIScript : MonoBehaviour {
 
     public Text LivesText;
     public Text DistanceText;
+
+    public RectTransform blackout;
+    public Text pauseText;
+    public RectTransform pausePanel;
+    public Button Resume;
+    public Button MainMenu;
+
+    private bool isPaused = false;
+    private float lastTimescale;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if(!isPaused)
+            {
+                PauseGame();
+            }
+            else
+            {
+                UnpauseGame();
+            }
+        }
+    }
+
+    private void PauseGame()
+    {
+        lastTimescale = Time.timeScale;
+        Time.timeScale = 0;
+        isPaused = true;
+
+        //Turn on pause UI
+        blackout.gameObject.SetActive(true);
+        pauseText.gameObject.SetActive(true);
+        pausePanel.gameObject.SetActive(true);
+        Resume.gameObject.SetActive(true);
+        MainMenu.gameObject.SetActive(true);
+    }
+
+    private void UnpauseGame()
+    {
+        Time.timeScale = lastTimescale;
+        isPaused = false;
+
+        //Turn off pause UI
+        blackout.gameObject.SetActive(false);
+        pauseText.gameObject.SetActive(false);
+        pausePanel.gameObject.SetActive(false);
+        Resume.gameObject.SetActive(false);
+        MainMenu.gameObject.SetActive(false);
+    }
     
     public void updateLives(int newLives)
     {
