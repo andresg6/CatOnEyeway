2a5b7d006bd0fc1bf59915352c2b174915e73602
andresg6 <andresg6@uci.edu>
2016-04-09 17:23:26 -0700

    Score Tracker Prefab; Scene Swap can now Exit the Game

---
+++ Assets/Scripts/ScoreTracker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreTracker : MonoBehaviour {
+    public UIScript mainUi;
+    private float timePassed;
+
+    void Start()
+    {
+        timePassed = 0;
+    }
+	void Update()
+    {
+        timePassed += Time.deltaTime;
+        mainUi.updateDistanceTraveled(timePassed / 4);
+    }
+
+    public void passScore()
+    {
+        PlayerPrefs.SetInt("Score", Mathf.FloorToInt(timePassed / 4));
+    }
+}
