7ffebf4a5233232e08c7653e01c281dfbae4605c
Kevin Wong <wong.kevin47@gmail.com>
2016-04-10 10:38:52 -0700

    Add dynamic check for TobiiEye

--- Assets/Scripts/FollowGaze.cs
+++ Assets/Scripts/FollowGaze.cs
@@ -5,26 +5,52 @@ public class FollowGaze : MonoBehaviour {
 
     public float lerpAmount;
 
+    private EyeXHost eyeXHost;
     private GazePointDataComponent gazePointDataComponent;
 
 	void Start () {
+        eyeXHost = EyeXHost.GetInstance();
         gazePointDataComponent = GetComponent<GazePointDataComponent>();
 	}
 	
 	void Update () {
-        EyeXGazePoint lastGazePoint = gazePointDataComponent.LastGazePoint;
+        EyeXDeviceStatus eyeTrackerDeviceStatus = eyeXHost.EyeTrackingDeviceStatus;
 
         if (!Input.GetKey(KeyCode.Space))
         {
-            if (lastGazePoint.IsWithinScreenBounds)
+            if (eyeTrackerDeviceStatus == EyeXDeviceStatus.Tracking)
             {
-                Vector2 screenSpace = lastGazePoint.Screen;
-
-                Vector3 gazeWorldPoint = Camera.main.ScreenToWorldPoint(new Vector3(screenSpace.x, screenSpace.y, Camera.main.nearClipPlane));
-                gazeWorldPoint = new Vector3(gazeWorldPoint.x, gazeWorldPoint.y, 0.0f);
-                Vector3 lerped = Vector3.Lerp(transform.position, gazeWorldPoint, lerpAmount);
-                transform.position = lerped;
+                FollowGazePoint();
+            }
+            else
+            {
+                FollowMouse();
             }
         }
 	}
+
+    void FollowGazePoint()
+    {
+        EyeXGazePoint lastGazePoint = gazePointDataComponent.LastGazePoint;
+
+        if (lastGazePoint.IsWithinScreenBounds)
+        {
+            Vector2 screenSpace = lastGazePoint.Screen;
+
+            Vector3 gazeWorldPoint = Camera.main.ScreenToWorldPoint(new Vector3(screenSpace.x, screenSpace.y, Camera.main.nearClipPlane));
+            gazeWorldPoint = new Vector3(gazeWorldPoint.x, gazeWorldPoint.y, 0.0f);
+            Vector3 lerped = Vector3.Lerp(transform.position, gazeWorldPoint, lerpAmount);
+            transform.position = lerped;
+        }
+    }
+
+    void FollowMouse()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        Vector3 gazeWorldPoint = Camera.main.ScreenToWorldPoint(mousePos);
+        gazeWorldPoint = new Vector3(gazeWorldPoint.x, gazeWorldPoint.y, 0.0f);
+        Vector3 lerped = Vector3.Lerp(transform.position, gazeWorldPoint, lerpAmount);
+        transform.position = lerped;
+    }
 }
